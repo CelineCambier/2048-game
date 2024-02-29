d442865f9922f9b5063d67fcf78df3ac3c7026f3
CelineCambier <cambier.celine2@gmail.com>
2024-02-28 17:37:22 -0800

    Added handleInput

--- grid.js
+++ grid.js
@@ -16,7 +16,22 @@ export default class Grid {
         Math.floor(index / GRID_SIZE)
       );
     });
-    console.log(this.cells);
+  }
+
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || [];
+      cellGrid[cell.x][cell.y] = cell;
+      return cellGrid;
+    }, []);
+  }
+
+  get cellsByRow() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.y] = cellGrid[cell.y] || [];
+      cellGrid[cell.y][cell.x] = cell;
+      return cellGrid;
+    }, []);
   }
 
   get #emptyCells() {
@@ -34,23 +49,50 @@ class Cell {
   #x;
   #y;
   #tile;
+  #mergeTile;
 
   constructor(cellElement, x, y) {
     this.#cellElement = cellElement;
     this.#x = x;
     this.#y = y;
-    }
-    
-    get tile() {
-        return this.#tile
-    }
+  }
 
-    set tile(value) {
-        this.#tile = value
-        if (value == null) return
-        this.#tile.x = this.#x
-        this.#tile.y = this.#y
-    }
+  get x() {
+    return this.#x;
+  }
+
+  get y() {
+    return this.#y;
+  }
+
+  get tile() {
+    return this.#tile;
+  }
+
+  set tile(value) {
+    this.#tile = value;
+    if (value == null) return;
+    this.#tile.x = this.#x;
+    this.#tile.y = this.#y;
+  }
+
+  get mergeTile() {
+    return this.#mergeTile;
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value;
+    if (value == null) return;
+    this.#mergeTile.x = this.#x;
+    this.#mergeTile.y = this.#y;
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergetile == null && this.tile.value === tile.value)
+    );
+  }
 }
 
 function createCellElements(gridElement) {
